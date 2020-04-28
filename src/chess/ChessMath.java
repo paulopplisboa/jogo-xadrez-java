524209e8d8e8df7b9e49c6f6488ff5475f55a08a
Paulo Silva <silva.pff@gmail.com>
2020-04-28 10:02:57 +0100

    Colocar a Rainha e o Rei no tabuleiro

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -1,13 +1,19 @@
 package chess;
 
+import java.awt.Color;
+
 import boardgame.Board;
+import boardgame.Position;
+import chess.piece.King;
+import chess.piece.Rook;
 
 public class ChessMath {
 	
 	private Board board;
 	
 	public ChessMath() 	{
-		board = new Board(8,8);		
+		board = new Board(8,8);	
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -22,5 +28,11 @@ public class ChessMath {
 	}
 	return mat;
 	
+	}
+	// método para colocar as peças no tabuleiro no inicio do jogo
+	private void initialSetup() {
+		board.placePiece(new Rook (board, chess.Color.WHIT), new Position(2,1));
+		board.placePiece(new King (board, chess.Color.BLACK), new Position (0,4));
+		board.placePiece(new King (board, chess.Color.WHIT), new Position (7,4));
 	}
 }
