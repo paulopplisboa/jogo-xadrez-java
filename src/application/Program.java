a611fa971e2ce649054dc101be68704a0bc767f4
Paulo Silva <silva.pff@gmail.com>
2020-04-29 10:56:24 +0100

    Criação do método para limpeza do ecrã

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -16,18 +18,26 @@ public class Program {
 		// Função para imprimir as peças da partida (Classe UI e metodo printBoard)
 
 		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMath.getPieces());
+				System.out.println();
+				System.out.print("Origem: ");
+				ChessPosition source = UI.readChessPosition(sc);
 
-			UI.printBoard(chessMath.getPieces());
-			System.out.println();
-			System.out.println("Origem: ");
-			ChessPosition source = UI.readChessPosition(sc);
+				System.out.println();
+				System.out.print("Destino: ");
+				ChessPosition target = UI.readChessPosition(sc);
 
-			System.out.println();
-			System.out.println("Destino: ");
-			ChessPosition target = UI.readChessPosition(sc);
-
-			ChessPiece capturePiece = chessMath.performChessMove(source, target);
-
+				ChessPiece capturePiece = chessMath.performChessMove(source, target);
+				
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 
